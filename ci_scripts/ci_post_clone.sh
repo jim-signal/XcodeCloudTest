d46f8fb133b177bcb8fefb0f29b800d7e06ace82
Jim Gustafson <jim@signal.org>
2026-07-13 13:12:25 -0700

    Fix path again

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -9,4 +9,4 @@ mkdir -p "$OUTPUT_DIR"
 
 # Execute the python script using Xcode's internal framework
 #python3 ./fetch-artifact.py -p ios --output-dir="$OUTPUT_DIR" --webrtc-version="$WEBRTC_VERSION" --archive-dir "$OUTPUT_DIR"
-env Accept-Encoding="identity" python3 ./ci_scripts/fetch-artifact.py -p ios --output-dir="$OUTPUT_DIR" --webrtc-version="$WEBRTC_VERSION" --archive-dir "$OUTPUT_DIR"
+env Accept-Encoding="identity" python3 ./fetch-artifact.py -p ios --output-dir="$OUTPUT_DIR" --webrtc-version="$WEBRTC_VERSION" --archive-dir "$OUTPUT_DIR"
