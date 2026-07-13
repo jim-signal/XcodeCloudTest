42ab5b3f7071530e5036f29c5a879eec6c272916
Jim Gustafson <jim@signal.org>
2026-07-13 13:04:23 -0700

    Add encoding header

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-echo "=== Phase 1: Attempting download WITHOUT fix ==="
+#echo "=== Phase 1: Attempting download WITHOUT fix ==="
+
+echo "=== Phase 1: Attempting download WITH fix ==="
 
 WEBRTC_VERSION="7871a"
 OUTPUT_DIR="./output"
 mkdir -p "$OUTPUT_DIR"
+
+export Accept-Encoding="identity"
 
 # Execute the python script using Xcode's internal framework
 python3 ./fetch-artifact.py -p ios --output-dir="$OUTPUT_DIR" --webrtc-version="$WEBRTC_VERSION" --archive-dir "$OUTPUT_DIR"
