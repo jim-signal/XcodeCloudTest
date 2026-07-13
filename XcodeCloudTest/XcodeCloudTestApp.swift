ebfad1ff923180593b518dc9679b34902df2bb81
Jim Gustafson <jim@signal.org>
2026-07-13 11:14:23 -0700

    Initial Commit

---
+++ XcodeCloudTest/XcodeCloudTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  XcodeCloudTestApp.swift
+//  XcodeCloudTest
+//
+//  Created by James Gustafson on 7/13/26.
+//
+
+import SwiftUI
+
+@main
+struct XcodeCloudTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
